81693e7360649755bdc859bfaab434f66ce0d301
Andrey Astanin <astanin-andrei@mail.ru>
2022-05-01 23:03:36 +0700

    feat: add resolve for tasks

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,9 +23,29 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  let result = []
+  for (let i = 0; i < matrix.length; i++) {
+    result[i] = []
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][j] == true) {
+        result[i][j] = 1
+      }
+      else {
+        let counter = 0
+        if (matrix[i-1] && matrix[i-1][j-1] === true) counter +=1
+        if (matrix[i-1] && matrix[i-1][j] === true) counter +=1
+        if (matrix[i-1] && matrix[i-1][j+1] === true) counter +=1
+        if (matrix[i][j-1] === true) counter +=1
+        if (matrix[i][j+1] === true) counter +=1
+        if (matrix[i+1] && matrix[i+1][j-1] === true) counter +=1
+        if (matrix[i+1] && matrix[i+1][j] === true) counter +=1
+        if (matrix[i+1] && matrix[i+1][j+1] === true) counter +=1
+        result[i][j] = counter
+      }
+    }
+  }
+  return result
 }
 
 module.exports = {
--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -11,9 +11,28 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function sortByHeight(/* arr */) {
+  const array = arguments[0]
+  let result = []
+  let values = []
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === -1) {
+      result[i] = array[i]
+    }
+    else {
+      result[i] = 'temp'
+      values.push(array[i])
+    }
+  }
+  console.log(values)
+  for (let i = 0; i < result.length; i++) {
+    if (result[i] != -1) {
+      let m = Math.min(...values)
+      result[i] = m
+      values.splice(values.indexOf(m), 1)
+    } 
+  }
+  return result
 }
 
 module.exports = {
