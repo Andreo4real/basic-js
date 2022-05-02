cef3cfcb718567df22cb4fc23483955f3c2c9f69
Andrey Astanin <astanin-andrei@mail.ru>
2022-05-02 14:46:22 +0700

    feat: add resolve for tasks

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -16,8 +16,43 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+  let str = arguments[0]
+  const options = arguments[1]
+  let strAddition
+  if (!options.hasOwnProperty('separator')) {
+    options.separator = '+'
+  }
+  if (!options.hasOwnProperty('repeatTimes')) {
+    options.repeatTimes = 1
+  }
+  if (!options.hasOwnProperty('additionSeparator')) {
+    options.additionSeparator = '|'
+  }
+  if (typeof(options.addition) == 'object' && typeof(str) == 'object' && !options.hasOwnProperty('additionRepeatTimes')) {
+    options.addition = 'STRING_OR_DEFAULT'
+  }
+
+  if (options.hasOwnProperty('addition') && options.hasOwnProperty('additionRepeatTimes') && options.hasOwnProperty('additionSeparator')) {
+    let temp = options.addition + options.additionSeparator
+    strAddition = temp.repeat(options.additionRepeatTimes).slice(0, -options.additionSeparator.length)
+  } else {
+      if (options.hasOwnProperty('addition') && options.hasOwnProperty('additionRepeatTimes')) {
+        strAddition = options.addition.repeat(options.additionRepeatTimes)
+      } 
+          if (options.hasOwnProperty('addition') && !options.hasOwnProperty('additionRepeatTimes'))  {
+            strAddition = options.addition
+          }
+        }
+  
+
+  if (typeof(strAddition) == 'string') {
+     str = str + strAddition + options.separator
+    } else {
+      str = str + options.separator
+    }
+  let result = str.repeat(options.repeatTimes).slice(0, -options.separator.length)
+
+  return result
 }
 
 module.exports = {
--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -12,10 +12,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  * depthCalc.calculateDepth([[[]]]) => 3
  *
  */
- class DepthCalculator {         
-  calculateDepth(array) {
+ class DepthCalculator {
+  // constructor() {
+  //   this.result = 1
+  // }
+  calculateDepth(/* arr */) {
     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    // let arr = arguments[0]
+    // for (let i = 0; i < arr.length; i++) {
+    //   if (Array.isArray(arr[i])) {
+    //     this.result += 1
+    //     if (i === arr.length - 1) {
+    //       return this.result
+    //     }
+    //     const flated = arr.flat()
+    //     this.calculateDepth(flated)
+    //   }
+    // }
 }
 }
 
